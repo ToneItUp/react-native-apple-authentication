6371ba92da7f712c5800eae6040ed7cf17ff122f
Jupiter Cat <jwebcat@gmail.com>
2020-03-24 12:19:18 -0700

    reformat js

--- index.js
+++ index.js
@@ -1,39 +1,45 @@
 import React from 'react';
-import { NativeModules, requireNativeComponent ,Platform } from 'react-native';
+import { NativeModules, requireNativeComponent, Platform } from 'react-native';
 
 const { AppleAuthentication } = NativeModules;
 
 export const RNSignInWithAppleButton = requireNativeComponent('RNCSignInWithAppleButton');
 
 export const SignInWithAppleButton = (buttonStyle, callBack) => {
-  if(Platform.OS === 'ios'){
-    return <RNSignInWithAppleButton style={buttonStyle} onPress={async () => {
-
-        await AppleAuthentication.requestAsync({
-          requestedScopes: [AppleAuthentication.Scope.FULL_NAME, AppleAuthentication.Scope.EMAIL],
-        }).then((response) => {
-          callBack(response) //Display response
-          }, (error) => {
-            callBack(error) //Display error
-
-        });
-
-  }} />
-  }else{
-  return null
-
-  }
-
-}
+	if (Platform.OS === 'ios') {
+		return (
+			<RNSignInWithAppleButton
+				style={buttonStyle}
+				onPress={async () => {
+					await AppleAuthentication.requestAsync({
+						requestedScopes: [ AppleAuthentication.Scope.FULL_NAME, AppleAuthentication.Scope.EMAIL ]
+					}).then(
+						(response) => {
+							callBack(response); //Display response
+						},
+						(error) => {
+							callBack(error); //Display error
+						}
+					);
+				}}
+			/>
+		);
+	} else {
+		return null;
+	}
+};
 
 export const AppleAuthenticationRequest = async (cb) => {
-  await AppleAuthentication.requestAsync({
-    requestedScopes: [AppleAuthentication.Scope.FULL_NAME, AppleAuthentication.Scope.EMAIL],
-  }).then((response) => {
-    cb(response)
-    }, (error) => {
-      cb(error)
-  });
-}
+	await AppleAuthentication.requestAsync({
+		requestedScopes: [ AppleAuthentication.Scope.FULL_NAME, AppleAuthentication.Scope.EMAIL ]
+	}).then(
+		(response) => {
+			cb(response);
+		},
+		(error) => {
+			cb(error);
+		}
+	);
+};
 
 export default AppleAuthentication;
